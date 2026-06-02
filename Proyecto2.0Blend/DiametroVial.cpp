3098cca848d20d5b5eb7a8d80b3ae4bcf7c5b4ac
Griz-8 <michelle.carvajal@ucb.edu.bo>
2026-06-02 19:24:18 -0400

    actualizacion

--- Proyecto2.0Blend/DiametroVial.cpp
+++ Proyecto2.0Blend/DiametroVial.cpp
@@ -2,6 +2,8 @@
 #include "Dijkstra.h"
 
 #include <limits>
+#include <random>
+#include <algorithm>
 
 using namespace std;
 
@@ -13,9 +15,87 @@ ResultadoDiametro DiametroVial::calcularDiametro(
     ResultadoDiametro resultado;
 
     resultado.distanciaMaxima = 0.0;
+    resultado.nodoOrigenReal = -1;
+    resultado.nodoDestinoReal = -1;
 
     for (int origenReal : nodosComponenteGigante)
     {
+        Dijkstra dijkstra;
+
+        ResultadoDijkstra resultadoDijkstra =
+            dijkstra.calcular(
+                grafo,
+                origenReal,
+                DISTANCIA
+            );
+
+        const vector<double>& distancia =
+            resultadoDijkstra.costo;
+
+        for (int i = 0; i < grafo.cantidadNodos(); i++)
+        {
+            if (
+                distancia[i] <
+                numeric_limits<double>::max()
+                &&
+                distancia[i] >
+                resultado.distanciaMaxima
+                )
+            {
+                resultado.distanciaMaxima =
+                    distancia[i];
+
+                resultado.nodoOrigenReal =
+                    origenReal;
+
+                resultado.nodoDestinoReal =
+                    grafo.obtenerIdReal(i);
+            }
+        }
+    }
+
+    return resultado;
+}
+
+ResultadoDiametro DiametroVial::calcularDiametroSampling(
+    const Grafo& grafo,
+    const vector<int>& nodosComponenteGigante,
+    int cantidadMuestras
+)
+{
+    ResultadoDiametro resultado;
+
+    resultado.distanciaMaxima = 0.0;
+    resultado.nodoOrigenReal = -1;
+    resultado.nodoDestinoReal = -1;
+
+    int totalNodos =
+        (int)nodosComponenteGigante.size();
+
+    if (totalNodos == 0)
+    {
+        return resultado;
+    }
+
+    cantidadMuestras =
+        min(cantidadMuestras, totalNodos);
+
+    random_device rd;
+    mt19937 gen(rd());
+
+    uniform_int_distribution<>
+        distribucion(0, totalNodos - 1);
+
+    for (int muestra = 0;
+        muestra < cantidadMuestras;
+        muestra++)
+    {
+        int indiceAleatorio =
+            distribucion(gen);
+
+        int origenReal =
+            nodosComponenteGigante[indiceAleatorio];
+
         Dijkstra dijkstra;
 
         ResultadoDijkstra resultadoDijkstra =
